d926ac17736699449cd16291fc2b140ff19a6e50
VasilVelinov <45295228+VasilVelinov@users.noreply.github.com>
2023-03-04 18:46:59 +0200

    Delete nginx directory

--- nginx/web/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-print "<h3>Hello Docker Swarm!</h3>\n";
-if (getenv('APP_MODE')) print "Running in ".getenv('APP_MODE')." mode.<br />\n";
-print "<hr />\n";
-print "<small><i>Served by: <b>".gethostname()."</b></i></small>\n";
-?>
